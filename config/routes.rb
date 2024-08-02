30f47c082db16ec0c21fa72b4960433bc4f4f63c
yasaigaoisi <yakkun075@gmail.com>
2024-08-02 15:48:40 +0900

    ホーム画面の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
-  root to: 'rooms#show'
+  root to: 'rooms#home'
   get 'rooms/show'
+  get 'rooms/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
